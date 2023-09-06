04f45aa9a6bd2774032fcd9b3c4519c2d5cb74c8
Shamimayesmin <shamimayesmin144@gmail.com>
2023-09-06 21:41:05 +0600

    edit contact by id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const { query } = require("express");
 require("dotenv").config();
 
@@ -12,33 +12,31 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ui8slz3.mongodb.net/?retryWrites=true&w=majority`;
 
 console.log(uri);
 
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+	serverApi: {
+		version: ServerApiVersion.v1,
+		strict: true,
+		deprecationErrors: true,
+	},
 });
 
 async function run() {
-  try {
-    const addContactCollection = client.db("Contact-app").collection("contacts");
+	try {
+		const addContactCollection = client
+			.db("Contact-app")
+			.collection("contacts");
 
-
-    // add contact
+		// add contact
 		app.post("/contacts", async (req, res) => {
 			const addContact = req.body;
 			// console.log(req.body);
 			const result = await addContactCollection.insertOne(addContact);
 			res.send(result);
 		});
-
-        
 
 		// get alll contact
 		app.get("/contacts", async (req, res) => {
@@ -50,18 +48,27 @@ async function run() {
 			// console.log(result);
 		});
 
-        app.get("/contact/:id", async (req, res) => {
-            const id = req.params.id;
-        
-            const result = await addContactCollection.findOne({ _id: new ObjectId(id) });
-            res.send(result);
-            // console.log(result);
-        });
-        
-    
-  } finally {
-    
-  }
+		app.get("/contact/:id", async (req, res) => {
+			const id = req.params.id;
+
+			const result = await addContactCollection.findOne({
+				_id: new ObjectId(id),
+			});
+			res.send(result);
+			// console.log(result);
+		});
+
+		//get specific contact edit
+		app.get("/edit/:id", async (req, res) => {
+			const id = req.params.id;
+			const query = {
+				_id: new ObjectId(id),
+			};
+			const result = await addContactCollection.findOne(query);
+			res.send(result);
+		});
+	} finally {
+	}
 }
 run().catch((err) => console.log(err));
 
