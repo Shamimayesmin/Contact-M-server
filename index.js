2adc42bb066bfa665d805a59a7c261ade71b0918
Shamimayesmin <shamimayesmin144@gmail.com>
2023-09-06 21:43:01 +0600

    delete contact

--- index.js
+++ index.js
@@ -90,6 +90,14 @@ async function run() {
 			);
 			res.send(result);
 		});
+        // delete contact
+        app.delete("/contact/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await addContactCollection.deleteOne(query);
+            res.send(result);
+        });
+        
 	} finally {
 	}
 }
