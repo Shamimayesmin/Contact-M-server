95fafd274f1b933783095ac731f8a97e82968f35
Shamimayesmin <shamimayesmin144@gmail.com>
2023-09-06 21:40:12 +0600

    get specific contact by id

--- index.js
+++ index.js
@@ -50,6 +50,13 @@ async function run() {
 			// console.log(result);
 		});
 
+        app.get("/contact/:id", async (req, res) => {
+            const id = req.params.id;
+        
+            const result = await addContactCollection.findOne({ _id: new ObjectId(id) });
+            res.send(result);
+            // console.log(result);
+        });
         
     
   } finally {
